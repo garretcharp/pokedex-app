0414c730a4957c9f6202da1e2a945ae76f249e9b
Garret Harp <garretcharp@gmail.com>
2023-09-07 22:22:22 -0500

    feat: Navigation layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+import { Separator } from "@/components/ui/separator";
+import Link from "next/link";
 
 export const metadata = {
-  title: "Next on Cloudflare",
-  description: "D1, KV in NextJS on Cloudflare Workers",
+  title: "Pokedex",
+  description: "",
 };
 
 export default function RootLayout({
@@ -14,9 +14,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={inter.className}>
-      <body>
-        <main className="flex min-h-screen items-center mx-auto flex-col p-24">
+    <html lang="en">
+      <body className="dark:bg-slate-700 dark:text-white">
+        <nav className="mt-5 flex h-5 items-center justify-center space-x-4 text-lg">
+          <Link href="/">Home</Link>
+          <Separator orientation="vertical" />
+          <Link href="/pokedex">Pokedex</Link>
+        </nav>
+
+        <main className="flex min-h-screen items-center mx-auto flex-col p-5">
           {children}
         </main>
       </body>
--- src/app/pokedex/page.tsx
+++ src/app/pokedex/page.tsx
@@ -1,10 +1,10 @@
 export const runtime = "edge";
 export const revalidate = 3600;
 
-export default async function Home() {
+export default async function Pokedex() {
   return (
     <h1 className="text-2xl mb-4 font-bold">
-      Hello pokemon listing!
+      Hello pokedex!
     </h1>
   );
 }
